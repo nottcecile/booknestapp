aeaae4d4f04f9a03f4cf43c9d6665381ea7c1ad7
David <oshilarusimi@gmail.com>
2025-05-17 05:30:37 -0700

    footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="w-full py-3 text-center text-sm text-pink-400 select-none">
-      Made with <span aria-label="heart" role="img">❤️</span> by Cecile
+    <footer className="w-full py-3 text-center text-sm text-purple-400 select-none">
+      Made with <span aria-label="heart" role="img">💜</span> by Cecile
     </footer>
   );
 }
